f1d593bddadc255fbff1da6af2b37b241073783b
anphu123 <anphu221002@gmail.com>
2023-05-16 18:00:06 +0700

    a

--- app/src/main/java/com/example/asm_andrnangcao_ps19353/model/MonHoc.java
+++ app/src/main/java/com/example/asm_andrnangcao_ps19353/model/MonHoc.java
@@ -3,5 +3,12 @@ package com.example.asm_andrnangcao_ps19353.model;
 public class MonHoc {
     private String code;
     private String name;
-    private String
+    private String teacher;
+
+
+    public MonHoc(String code, String name, String teacher) {
+        this.code = code;
+        this.name = name;
+        this.teacher = teacher;
+    }
 }
