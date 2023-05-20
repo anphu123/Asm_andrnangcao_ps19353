a319537ae6e1d4e85de44cda0d374655458ee248
anphu123 <anphu221002@gmail.com>
2023-05-21 06:51:15 +0700

    a

--- app/src/main/java/com/example/asm_andrnangcao_ps19353/MainActivity.java
+++ app/src/main/java/com/example/asm_andrnangcao_ps19353/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.asm_andrnangcao_ps19353;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.asm_andrnangcao_ps19353.dao.DangKyMonHocDAO;
 import com.example.asm_andrnangcao_ps19353.model.MonHoc;
@@ -16,8 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DangKyMonHocDAO dao = new DangKyMonHocDAO(this);
-        ArrayList<MonHoc> list = dao.getDSMonHoc();
-        String a = "";
+        ImageView imgCourse= (ImageView) findViewById(R.id.imgCourse);
+        ImageView imgMap= (ImageView) findViewById(R.id.imgMap);
+        ImageView imgNews= (ImageView) findViewById(R.id.imgNews);
+        ImageView imgSocial= (ImageView) findViewById(R.id.imgSocial);
+
+        imgMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        imgNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,NewsActivity.class);
+                startActivity(intent);
+            }
+        });
+        imgSocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SocialActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,CourseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/asm_andrnangcao_ps19353/SocialActivity.java
@@ -0,0 +1,14 @@
+package com.example.asm_andrnangcao_ps19353;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social);
+    }
+}
