d979aa633d7a75c018a58e6b016fa7fc61069042
anphu123 <anphu221002@gmail.com>
2023-05-20 23:58:42 +0700

    a

--- app/src/main/java/com/example/asm_andrnangcao_ps19353/Manhinhchao.java
+++ app/src/main/java/com/example/asm_andrnangcao_ps19353/Manhinhchao.java
@@ -3,6 +3,10 @@ package com.example.asm_andrnangcao_ps19353;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Manhinhchao extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class Manhinhchao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhchao);
+
+        ImageView ivLogo = (ImageView) findViewById(R.id.ivLogo);
+
+
+        Glide.with(this).load(R.mipmap.splash).into(ivLogo);
     }
 }
